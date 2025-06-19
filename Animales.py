5e9483ad711dbee9dbb4f83ef551ff1d2cb73999
SanKael <santiagoleon75@gmail.com>
2025-06-19 10:17:25 +0000

    Práctica en solitario

--- Animales.py
+++ Animales.py
@@ -1,2 +1,11 @@
-print("¡Hola! Vamos a jugar con los animales."
+print("¡Hola! Vamos a jugar con los animales.")
 favorito = input("¿Cuál es tu animal favorito?\n")
+if favorito.lower() == "perro":
+    print("¡Guau! Los perros son geniales.")  
+elif favorito.lower() == "gato":
+    print("¡Miau! Los gatos son muy divertidos.")
+elif favorito.lower() == "pez":
+    print("¡Glub! Los peces son muy interesantes.")
+else:
+    print(f"¡{favorito.capitalize()}! Es un animal muy especial.")
+print ("Gracias por jugar conmigo. ¡Adiós!")
