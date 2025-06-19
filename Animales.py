d6f5df1914aacbc2e94b88834f75e671ef05e4fb
SanKael <santiagoleon75@gmail.com>
2025-06-19 10:42:39 +0000

    Lista + bucle

--- Animales.py
+++ Animales.py
@@ -1,11 +1,27 @@
 print("¡Hola! Vamos a jugar con los animales.")
+
+# Lista de animales aceptados
+animales = ["perro", "gato", "pez"]
+
+# Input del usuario
 favorito = input("¿Cuál es tu animal favorito?\n")
-if favorito.lower() == "perro":
-    print("¡Guau! Los perros son geniales.")  
-elif favorito.lower() == "gato":
-    print("¡Miau! Los gatos son muy divertidos.")
-elif favorito.lower() == "pez":
-    print("¡Glub! Los peces son muy interesantes.")
-else:
+
+# Variable bandera para saber si encontramos coincidencia
+encontrado = False
+
+# Bucle para recorrer la lista
+for animal in animales:
+    if favorito.lower() == animal:
+        if animal == "perro":
+            print("¡Guau! Los perros son geniales.")
+        elif animal == "gato":
+            print("¡Miau! Los gatos son muy divertidos.")
+        elif animal == "pez":
+            print("¡Glub! Los peces son muy interesantes.")
+        encontrado = True
+
+# Si no está en la lista
+if not encontrado:
     print(f"¡{favorito.capitalize()}! Es un animal muy especial.")
-print ("Gracias por jugar conmigo. ¡Adiós!")
+
+print("Gracias por jugar conmigo. ¡Adiós!")
