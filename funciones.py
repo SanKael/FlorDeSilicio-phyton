fa448a4c9d6df159b236f3a06f8b0b51b69c5d05
SanKael <santiagoleon75@gmail.com>
2025-06-20 12:59:48 +0000

    Práctica de funciones

--- funciones.py
+++ funciones.py
@@ -1,5 +1,22 @@
 def saludar():
-    print("Buenas ¿Como estás.")
+    print("Buenos días.")
     nombre = input("¿Como te llamas?\n")
     return nombre
-    
+
+def estado():
+    estado = input("¿Como te sientes hoy?\n")
+    if estado.lower() == "triste":
+        print("Siento que estés triste. Estoy aquí contigo 🖤")
+    elif estado.lower() == "feliz":
+        print("¡Qué alegría! Que esa luz te dure mucho 🌞")
+    else:
+        print(f"Entiendo que te sientes {estado}. Gracias por compartirlo 🌿")
+    return estado
+
+def despedida():
+    print("Gracias por charlar conmigo. Vuelve cuando quieras. Que tu día florezca 🌸")
+    return despedida
+
+saludar()
+estado() 
+despedida()
