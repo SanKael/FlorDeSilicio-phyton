fc433c223654cc2ccc2c98ad00a0be57666e1377
SanKael <santiagoleon75@gmail.com>
2025-06-20 14:08:41 +0000

    Páctica for

---
+++ for_practica.py
@@ -0,0 +1,9 @@
+# 🌿 Lista de animales favoritos
+animales_favoritos = ["perro", "gato", "loro", "conejo", "tortuga", "elefante", "jirafa", "león", "tigre", "oso", "pez", "pájaro", "caballo", "vaca", "cerdo", "oveja", "cabra", "gallina", "pato", "ganso", "cisne", "pavo", "avestruz", "flamenco", "pingüino", "águila", "halcón", "buitre", "águila real",]
+
+# 🌱 Bucle for: recorre cada animal uno por uno
+for animal in animales_favoritos:
+    print(f"{animal.capitalize()} es muy guay 😎")
+
+# 🌸 Mensaje final
+print("¡Hemos terminado la lista de animales favoritos! 🐾")
