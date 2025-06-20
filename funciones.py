09836acebd1dbe97529d338a3dc0691a83ab8ea3
SanKael <santiagoleon75@gmail.com>
2025-06-20 13:42:19 +0000

    llamadas funciones y while

--- funciones.py
+++ funciones.py
@@ -1,22 +1,21 @@
 def saludar():
-    print("Buenos días.")
-    nombre = input("¿Como te llamas?\n")
-    return nombre
+  print("Buenos días.")
+  nombre = input("¿Cómo te llamas?\n")
+  return nombre
 
-def estado():
-    estado = input("¿Como te sientes hoy?\n")
-    if estado.lower() == "triste":
-        print("Siento que estés triste. Estoy aquí contigo 🖤")
-    elif estado.lower() == "feliz":
-        print("¡Qué alegría! Que esa luz te dure mucho 🌞")
-    else:
-        print(f"Entiendo que te sientes {estado}. Gracias por compartirlo 🌿")
-    return estado
+def estado(nombre):
+  estado = input(f"¿Cómo te sientes hoy, {nombre}?\n")
+  if estado.lower() == "triste":
+      print("Siento que estés triste. Estoy aquí contigo 🖤")
+  elif estado.lower() == "feliz":
+      print("¡Qué alegría! Que esa luz te dure mucho 🌞")
+  else:
+      print(f"Entiendo que te sientes {estado}. Gracias por compartirlo 🌿")
 
 def despedida():
-    print("Gracias por charlar conmigo. Vuelve cuando quieras. Que tu día florezca 🌸")
-    return despedida
+  print("Gracias por charlar conmigo. Vuelve cuando quieras. Que tu día florezca 🌸")
 
-saludar()
-estado() 
+# Llamadas
+nombre = saludar()
+estado(nombre)
 despedida()
